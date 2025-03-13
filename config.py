7330f2d1c5e3ff86dc2955848e48dac0b2c0d9d6
kamaldhankharts <kamaldhankhararts@gmail.com>
2025-03-13 10:22:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+FLYa3k5UpTU4Yjk1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+vOU95CXaBPswN2U1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/wordy_Maze_7")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+0O-MJaBj-MBlMzk1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -89,10 +89,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/8newbf.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/8newbf.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/mo2u0v.jpg"
